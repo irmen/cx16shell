irmen/cx16shell

commit b1d6274dfe73e956fc3dd02ea0a28d92c4406710

added motd and time

src/disk_commands.p8
@@ -3,23 +3,24 @@
 %import diskio
 %import conv
 %import errors
+%encoding iso
 
 disk_commands {
 
     uword[] commands_table = [
-        iso:"ls", &cmd_ls,
-        iso:"cat", &cmd_cat,
-        iso:"rm", &cmd_rm,
-        iso:"del", &cmd_rm,
-        iso:"mv", &cmd_rename,
-        iso:"ren", &cmd_rename,
-        iso:"cp", &cmd_copy,
-        iso:"cd", &cmd_cd,
-        iso:"pwd", &cmd_pwd,
-        iso:"mkdir", &cmd_mkdir,
-        iso:"rmdir", &cmd_rmdir,
-        iso:"relabel", &cmd_relabel,
-        iso:"drive", &cmd_drive
+        "ls", &cmd_ls,
+        "cat", &cmd_cat,
+        "rm", &cmd_rm,
+        "del", &cmd_rm,
+        "mv", &cmd_rename,
+        "ren", &cmd_rename,
+        "cp", &cmd_copy,
+        "cd", &cmd_cd,
+        "pwd", &cmd_pwd,
+        "mkdir", &cmd_mkdir,
+        "rmdir", &cmd_rmdir,
+        "relabel", &cmd_relabel,
+        "drive", &cmd_drive
     ]
 
     sub recognized(str cmdword, ubyte length) -> uword {
@@ -35,14 +36,14 @@ disk_commands {
         ubyte num_files = 0
         if diskio.lf_start_list(main.command_arguments_ptr) {
             txt.color(main.COLOR_HIGHLIGHT)
-            txt.print(iso:" Blocks  Filename\r")
+            txt.print(" Blocks  Filename\r")
             txt.color(main.COLOR_NORMAL)
             while diskio.lf_next_entry() {
                 num_files++
                 txt.spc()
                 txt.spc()
                 if diskio.list_filetype == "dir"
-                    txt.print(iso:"[dir]")
+                    txt.print("[dir]")
                 else
                     main.print_uw_right(diskio.list_blocks)
                 txt.spc()
@@ -51,7 +52,7 @@ disk_commands {
                 txt.nl()
                 if cbm.STOP2() {
                     txt.color(main.COLOR_HIGHLIGHT)
-                    txt.print(iso:"Break\r")
+                    txt.print("Break\r")
                     txt.color(main.COLOR_NORMAL)
                     break
                 }
@@ -59,28 +60,28 @@ disk_commands {
             diskio.lf_end_list()
             if num_files == 0 {
                 txt.color(main.COLOR_HIGHLIGHT)
-                txt.print(iso:"No files\r")
+                txt.print("No files\r")
                 txt.color(main.COLOR_NORMAL)
             }
             return true
         }
-        return err.set(iso:"IO error")
+        return err.set("IO error")
     }
 
     sub cmd_rm() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: filename")
+            return err.set("Missing arg: filename")
         void string.find(main.command_arguments_ptr, '*')
         if_cs {
             ;txt.color(main.COLOR_HIGHLIGHT)
-            ;txt.print(iso:"Has * wildcard. Sure y/n? ")
+            ;txt.print("Has * wildcard. Sure y/n? ")
             ;txt.color(main.COLOR_NORMAL)
             ;ubyte answer = c64.CHRIN()
             ;txt.nl()
-            ;if answer == iso:'y' {
-            ;    return err.set(iso:"TODO custom pattern matching with * wildcard") ; TODO
+            ;if answer == 'y' {
+            ;    return err.set("TODO custom pattern matching with * wildcard") ; TODO
             ;}
-            return err.set(iso:"Refused to act on * wildcard")
+            return err.set("Refused to act on * wildcard")
         }
         diskio.delete(main.command_arguments_ptr)
         print_disk_status()
@@ -89,10 +90,10 @@ disk_commands {
 
     sub cmd_rename() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing args: oldfilename newfilename")
+            return err.set("Missing args: oldfilename newfilename")
 
         uword newfilename
-        ubyte space_idx = string.find(main.command_arguments_ptr, iso:' ')
+        ubyte space_idx = string.find(main.command_arguments_ptr, ' ')
         if_cs {
             newfilename = main.command_arguments_ptr + space_idx + 1
             main.command_arguments_ptr[space_idx] = 0
@@ -100,7 +101,7 @@ disk_commands {
             print_disk_status()
             return true
         } else {
-            return err.set(iso:"Missing args: oldfilename newfilename")
+            return err.set("Missing args: oldfilename newfilename")
         }
     }
 
@@ -113,7 +114,7 @@ disk_commands {
 
     sub cmd_cat() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: filename")
+            return err.set("Missing arg: filename")
 
         if diskio.f_open(main.command_arguments_ptr) {
             uword line = 0
@@ -144,9 +145,9 @@ disk_commands {
 
     sub cmd_pwd() -> bool {
         if main.command_arguments_size
-            return err.set(iso:"Has no args")
+            return err.set("Has no args")
         txt.color(main.COLOR_HIGHLIGHT)
-        txt.print(iso:"Drive number: ")
+        txt.print("Drive number: ")
         txt.color(main.COLOR_NORMAL)
         txt.print_ub(diskio.drivenumber)
         txt.nl()
@@ -193,7 +194,7 @@ io_error:
         cbm.CLOSE(12)
 
         if status and status & $40 == 0            ; bit 6=end of file
-            return err.set(iso:"IO error")
+            return err.set("IO error")
 
         txt.nl()
         return true
@@ -201,7 +202,7 @@ io_error:
         sub process_line(uword lineptr, bool diskname) {
             if diskname {
                 txt.color(main.COLOR_HIGHLIGHT)
-                txt.print(iso:"Disk name: ")
+                txt.print("Disk name: ")
                 txt.color(main.COLOR_NORMAL)
             }
             repeat {
@@ -213,11 +214,11 @@ io_error:
             }
             if diskname {
                 txt.color(main.COLOR_HIGHLIGHT)
-                txt.print(iso:"\rCurrent dir: ")
+                txt.print("\rCurrent dir: ")
                 txt.color(main.COLOR_NORMAL)
             } else if @(lineptr-1)!='/' {
                 txt.color(main.COLOR_HIGHLIGHT)
-                txt.print(iso:" in ")
+                txt.print(" in ")
                 txt.color(main.COLOR_NORMAL)
             }
         }
@@ -225,7 +226,7 @@ io_error:
 
     sub cmd_mkdir() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: dirname")
+            return err.set("Missing arg: dirname")
 
         diskio.list_filename[0] = 'm'
         diskio.list_filename[1] = 'd'
@@ -237,7 +238,7 @@ io_error:
 
     sub cmd_cd() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: dirname")
+            return err.set("Missing arg: dirname")
         diskio.chdir(main.command_arguments_ptr)
         print_disk_status()
         return true
@@ -245,11 +246,11 @@ io_error:
 
     sub cmd_rmdir() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: dirname")
+            return err.set("Missing arg: dirname")
 
         void string.find(main.command_arguments_ptr, '*')
         if_cs
-            return err.set(iso:"Refused to act on * wildcard")
+            return err.set("Refused to act on * wildcard")
 
         diskio.rmdir(main.command_arguments_ptr)
         print_disk_status()
@@ -258,7 +259,7 @@ io_error:
 
     sub cmd_relabel() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: diskname")
+            return err.set("Missing arg: diskname")
         diskio.relabel(main.command_arguments_ptr)
         print_disk_status()
         return true
@@ -266,10 +267,10 @@ io_error:
 
     sub cmd_copy() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing args: oldfilename newfilename")
+            return err.set("Missing args: oldfilename newfilename")
 
         uword newfilename
-        ubyte space_idx = string.find(main.command_arguments_ptr, iso:' ')
+        ubyte space_idx = string.find(main.command_arguments_ptr, ' ')
         if_cs {
             newfilename = main.command_arguments_ptr + space_idx + 1
             main.command_arguments_ptr[space_idx] = 0
@@ -282,25 +283,25 @@ io_error:
             print_disk_status()
             return true
         } else {
-            return err.set(iso:"Missing args: oldfilename newfilename")
+            return err.set("Missing args: oldfilename newfilename")
         }
     }
 
     sub cmd_drive() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: drive number")
+            return err.set("Missing arg: drive number")
 
         ubyte nr = conv.str2ubyte(main.command_arguments_ptr)
 
         when nr {
             8, 9 -> {
-                txt.print(iso:"Switching drive.\r")
+                txt.print("Switching drive.\r")
                 diskio.drivenumber = nr
                 main.command_arguments_size = 0
                 return cmd_pwd()
             }
             else -> {
-                return err.set(iso:"Invalid drive number")
+                return err.set("Invalid drive number")
             }
         }
     }

Makefile
@@ -1,6 +1,6 @@
 .PHONY:  all clean emu
 
-all:  shell.prg ext-command.prg neofetch.prg
+all:  shell.prg ext-command.prg neofetch.prg time.prg
 
 clean:
 	rm -f *.prg *.asm *.vice-*
@@ -9,6 +9,9 @@ emu:  all
 	mcopy -D o shell.prg x:SHELL
 	mcopy -D o ext-command.prg x:SHELL-CMDS/EXT-COMMAND
 	mcopy -D o neofetch.prg x:SHELL-CMDS/NEOFETCH
+	mcopy -D o time.prg x:SHELL-CMDS/TIME
+	mcopy -D o time.prg x:SHELL-CMDS/DATE
+	mcopy -D o motd.txt x:SHELL-CMDS/motd.txt
 	x16emu -sdcard ~/cx16sdcard.img -scale 2 -quality best -run -prg shell.prg
 
 shell.prg: src/shell.p8 src/errors.p8 src/disk_commands.p8 src/misc_commands.p8
@@ -20,3 +23,5 @@ ext-command.prg: externalcommands/example/ext-command.p8
 neofetch.prg: externalcommands/neofetch/neofetch.p8
 	p8compile $< -target cx16
 
+time.prg: externalcommands/example/time.p8
+	p8compile $< -target cx16

externalcommands/example/ext-command.p8
@@ -3,6 +3,7 @@
 %launcher none
 %option no_sysinit
 %zeropage basicsafe
+%encoding iso
 %address $4000
 
 shell {
@@ -29,26 +30,26 @@ main $4000 {
     %option force_output
 
     sub start()  {
-        shell.shell_print(iso:"This is an external command program!\rinvoked command: ")
+        shell.shell_print("This is an external command program!\rinvoked command: ")
         shell.shell_print(cx16.r0)        ; command
-        cbm.CHROUT(iso:' ')
+        cbm.CHROUT(' ')
         shell.shell_print_uw(cx16.r1)     ; length of command
         if cx16.r3 {
-            shell.shell_print(iso:"\rargs: ")
+            shell.shell_print("\rargs: ")
             shell.shell_print(cx16.r2)        ; arguments
-            shell.shell_print(iso:"\rargs length=")
+            shell.shell_print("\rargs length=")
             shell.shell_print_uw(cx16.r3)     ; length of arguments
         }
-        shell.shell_print(iso:"\renter name: ")
+        shell.shell_print("\renter name: ")
         str inputbuffer = "?"*20
         if shell.shell_input_chars(inputbuffer) {
-            shell.shell_print(iso:"\rinput was: ")
+            shell.shell_print("\rinput was: ")
             shell.shell_print(inputbuffer)
-            cbm.CHROUT(iso:'\r')
+            cbm.CHROUT('\r')
         }
         shell.shell_print_uwbin(12345, true)
-        cbm.CHROUT(iso:'\r')
+        cbm.CHROUT('\r')
         sys.exit(0)
-        ; void shell.shell_err_set(iso:"external command failed")
+        ; void shell.shell_err_set("external command failed")
     }
 }

externalcommands/example/time.p8
@@ -1,0 +1,84 @@
+; Show the current date and time
+
+%import textio
+%import syslib
+%launcher none
+%option no_sysinit
+%zeropage basicsafe
+%encoding iso
+%address $4000
+
+shell {
+    romsub $06e0 = print(str string @AY) clobbers(A,Y)
+    romsub $06e3 = print_uw(uword value @AY) clobbers(A,Y)
+    romsub $06e6 = print_uwhex(uword value @ AY, bool prefix @ Pc) clobbers(A,Y)
+    romsub $06e9 = print_uwbin(uword value @ AY, bool prefix @ Pc) clobbers(A,Y)
+    romsub $06ec = input_chars(uword buffer @ AY) clobbers(A) -> ubyte @Y
+    romsub $06ef = err_set(str message @AY) clobbers(Y) -> bool @A
+
+    ; input registers set by shell upon calling your command:
+    ;    cx16.r0 = command address
+    ;    cx16.r1 = length of command (byte)
+    ;    cx16.r2 = arguments address
+    ;    cx16.r3 = length of arguments (byte)
+
+    ; command should return error status in A. You can use err_set() to set a specific error message for the shell.
+    ; command CAN use the FREE zero page locations.
+    ; command CANNOT use memory below $4000 (the shell sits there)
+    ; command CAN use Ram $0400-$06df.
+}
+
+main $4000 {
+    %option force_output
+
+    sub start()  {
+        str[13] months = [0, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        str[7] days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+        shell.print("Current date and time (from RTC clock):\r")
+        ; romsub $ff50 = clock_get_date_time()  clobbers(A, X, Y)  -> uword @R0, uword @R1, uword @R2, uword @R3   ; result registers see clock_set_date_time()
+        cx16.clock_get_date_time()      ; uword yearmonth @R0, uword dayhours @R1, uword minsecs @R2, uword jiffiesweekday @R3
+        uword year = 1900 + cx16.r0L
+        ubyte month = cx16.r0H
+        ubyte day = cx16.r1L
+        ubyte hour = cx16.r1H
+        ubyte minutes = cx16.r2L
+        ubyte seconds = cx16.r2H
+        ubyte weekday = cx16.r3H
+        shell.print(days[weekday])
+        shell.print(", ")
+        shell.print(months[month])
+        shell.print(" ")
+        shell.print_uw(day)
+        shell.print(", ")
+        shell.print_uw(year)
+        shell.print(".  ")
+        if hour<10
+            shell.print("0")
+        shell.print_uw(hour)
+        shell.print(":")
+        if minutes<10
+            shell.print("0")
+        shell.print_uw(minutes)
+        shell.print(":")
+        if seconds<10
+            shell.print("0")
+        shell.print_uw(seconds)
+        cbm.CHROUT('\r')
+
+;        ubyte clock_lo
+;        ubyte clock_mid
+;        ubyte clock_hi
+;        %asm {{
+;            jsr  cbm.RDTIM
+;            sta  p8_clock_lo
+;            stx  p8_clock_mid
+;            sty  p8_clock_hi
+;        }}
+;        shell.print("\rJiffy clock: ")
+;        shell.print_uwhex(clock_hi, true)
+;        shell.print_uwhex(mkword(clock_mid, clock_lo), false)
+;        cbm.CHROUT('\r')
+        sys.exit(0)
+    }
+}

src/misc_commands.p8
@@ -1,21 +1,25 @@
 %import textio
 %import errors
 %import conv
+%encoding iso
 
 misc_commands {
 
     uword[] commands_table = [
-        iso:"basic", &cmd_basic,
-        iso:"exit", &cmd_basic,
-        iso:"help", &cmd_help,
-        iso:"num", &cmd_printnumber,
-        iso:"run", &cmd_run,
-        iso:"vi", &cmd_edit,
-        iso:"pico", &cmd_edit,
-        iso:"nano", &cmd_edit,
-        iso:"mem", &cmd_mem,
-        iso:"cls", &cmd_cls
+        "basic", &cmd_basic,
+        "exit", &cmd_basic,
+        "help", &cmd_help,
+        "motd", &cmd_motd,
+        "num", &cmd_printnumber,
+        "run", &cmd_run,
+        "vi", &cmd_edit,
+        "pico", &cmd_edit,
+        "nano", &cmd_edit,
+        "mem", &cmd_mem,
+        "cls", &cmd_cls
     ]
+
+    str motd_file = "//shell-cmds/:motd.txt"
 
     sub recognized(str cmdword, ubyte length) -> uword {
         ubyte idx
@@ -35,7 +39,7 @@ misc_commands {
 
     sub cmd_run() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: filename")
+            return err.set("Missing arg: filename")
 
         uword real_filename_ptr = main.file_lookup_matching(main.command_arguments_ptr, true)
         if real_filename_ptr {
@@ -43,7 +47,7 @@ misc_commands {
             return true
         }
         if not err.error_status
-            return err.set(iso:"File not found")
+            return err.set("File not found")
         return false
     }
 
@@ -53,15 +57,15 @@ misc_commands {
     }
 
     sub cmd_mem() -> bool {
-        txt.print(iso:"Shell prg: ")
+        txt.print("Shell prg: ")
         txt.print_uwhex(cbm.MEMBOT(0, true), true)
-        txt.chrout(iso:'-')
+        txt.chrout('-')
         txt.print_uwhex(sys.progend(), true)
-        txt.print(iso:"\rRam banks: ")
+        txt.print("\rRam banks: ")
         txt.print_uw(cx16.numbanks())
-        txt.chrout(iso:'=')
+        txt.chrout('=')
         txt.print_uw(cx16.numbanks() * $0008)
-        txt.print(iso:"KB\rMemTop: ")
+        txt.print("KB\rMemTop: ")
         txt.print_uwhex(cbm.MEMTOP(0, true), true)
         txt.nl()
         return true
@@ -69,7 +73,7 @@ misc_commands {
 
     sub cmd_printnumber() -> bool {
         if main.command_arguments_size==0
-            return err.set(iso:"Missing arg: number (any prefix)")
+            return err.set("Missing arg: number (any prefix)")
 
         if conv.any2uword(main.command_arguments_ptr) {
             txt.spc()
@@ -81,13 +85,23 @@ misc_commands {
             txt.nl()
             return true
         } else {
-            return err.set(iso:"Invalid number")
+            return err.set("Invalid number")
         }
+    }
+
+    sub cmd_motd() -> bool {
+        txt.color(main.COLOR_HIGHLIGHT)
+        txt.print("Message Of The Day (motd.txt):\r")
+        txt.color(main.COLOR_NORMAL)
+        main.command_arguments_ptr = &motd_file
+        main.command_arguments_size = string.length(motd_file)
+        disk_commands.cmd_cat()
+        return true
     }
 
     sub cmd_help() -> bool {
         txt.color(main.COLOR_HIGHLIGHT)
-        txt.print(iso:"Builtin Commands:\r")
+        txt.print("Builtin Commands:\r")
         txt.color(main.COLOR_NORMAL)
         ubyte idx
         for idx in 0 to len(misc_commands.commands_table)-1 step 2 {
@@ -102,11 +116,11 @@ misc_commands {
             txt.spc()
         }
         txt.color(main.COLOR_HIGHLIGHT)
-        txt.print(iso:"\rCommands on disk:\r")
+        txt.print("\rCommands on disk:\r")
         txt.color(main.COLOR_NORMAL)
-        txt.print(iso:"Type the name of an external command program located in 'SHELL-CMDS'\r  subdirectory (see documentation).\r")
-        txt.print(iso:"Or simply type name of program to launch (no suffix req'd, case insens.).\r")
-        txt.print(iso:"Typing the name of a directory (including '..'), cd's into that dir.\r")
+        txt.print("Type the name of an external command program located in 'SHELL-CMDS'\r  subdirectory (see documentation).\r")
+        txt.print("Or simply type name of program to launch (no suffix req'd, case insens.).\r")
+        txt.print("Typing the name of a directory (including '..'), cd's into that dir.\r")
         return true
     }
 
@@ -126,7 +140,7 @@ misc_commands {
                 void cx16.screen_mode(1, false)     ; back to shell's screen mode
                 return true
             } else {
-                return err.set(iso:"no x16edit in rom and no x16edit-6000.prg on disk")
+                return err.set("no x16edit in rom and no x16edit-6000.prg on disk")
             }
         }
 

src/shell.p8
@@ -4,6 +4,7 @@
 %import errors
 %import disk_commands
 %import misc_commands
+%encoding iso
 %zeropage basicsafe
 
 main {
@@ -12,7 +13,6 @@ main {
     const ubyte COLOR_HIGHLIGHT_PROMPT = 13
     const ubyte COLOR_ERROR = 10
     const ubyte COLOR_BACKGROUND = 11
-
     str command_line = "?" * 160
     str command_word = "?" * 64
     ubyte command_word_size
@@ -30,7 +30,7 @@ main {
         repeat {
             txt.color(COLOR_HIGHLIGHT_PROMPT)
             txt.nl()
-            txt.print(iso:"$ ")
+            txt.print("$ ")
             txt.color(COLOR_NORMAL)
             err.clear()
             ubyte input_size = txt.input_chars(command_line)
@@ -45,7 +45,7 @@ main {
                         if cx16.r0L!=0
                             err.clear()
                         else if not err.error_status {
-                            void err.set(iso:"Unspecified error")
+                            void err.set("Unspecified error")
                         }
                     } else {
                         ; see if there is an external shell command in the SHELL-CMDS subdirectory that matches
@@ -55,7 +55,7 @@ main {
                             void run_external_command()
                         else {
                             if command_line==".." {
-                                txt.print(iso:"cd into directory. ")
+                                txt.print("cd into directory. ")
                                 command_arguments_ptr = ".."
                                 command_arguments_size = string.length(command_arguments_ptr)
                                 void disk_commands.cmd_cd()
@@ -65,7 +65,7 @@ main {
                                 if real_filename_ptr {
                                     command_word = real_filename_ptr
                                     if is_directory(command_word) {
-                                        txt.print(iso:"cd into directory. ")
+                                        txt.print("cd into directory. ")
                                         command_arguments_ptr = command_word
                                         command_arguments_size = string.length(command_arguments_ptr)
                                         void disk_commands.cmd_cd()
@@ -74,12 +74,12 @@ main {
                                     }
                                 }
                                 else
-                                    void err.set(iso:"Invalid command")
+                                    void err.set("Invalid command")
                             }
                         }
                     }
                 } else {
-                    void err.set(iso:"Invalid input")
+                    void err.set("Invalid input")
                 }
             }
         }
@@ -90,17 +90,17 @@ main {
         ; replace Shift-SPACE by just normal SPACE
         while @(cmd_ptr) {
             if @(cmd_ptr)==$a0
-                @(cmd_ptr)=iso:' '
+                @(cmd_ptr)=' '
             cmd_ptr++
         }
         ; skip leading spaces
         cmd_ptr = &command_line
-        while @(cmd_ptr)==iso:' ' {
+        while @(cmd_ptr)==' ' {
             cmd_ptr++
             length--
         }
 
-        ubyte space_idx = string.find(cmd_ptr, iso:' ')
+        ubyte space_idx = string.find(cmd_ptr, ' ')
         if_cs {
             cmd_ptr[space_idx] = 0
             command_arguments_ptr = cmd_ptr + space_idx + 1
@@ -118,10 +118,18 @@ main {
     sub print_intro() {
         txt.color2(COLOR_NORMAL, COLOR_BACKGROUND)
         txt.clear_screen()
+
+        if diskio.f_open(misc_commands.motd_file) {
+            diskio.f_close()
+            misc_commands.cmd_motd()
+        } else {
+            diskio.send_command(petscii:"i")
+        }
+
         txt.color(COLOR_HIGHLIGHT_PROMPT)
-        txt.print(iso:"\r  Commander-X16 SHELL ")
+        txt.print("\r  Commander-X16 SHELL v1.1 ")
         txt.color(COLOR_NORMAL)
-        txt.print(iso:"- https://github.com/irmen/cx16shell\r")
+        txt.print("- https://github.com/irmen/cx16shell\r")
     }
 
     sub file_lookup_matching(uword filename_ptr, bool only_programs) -> uword {
@@ -148,11 +156,11 @@ main {
                             diskio.lf_end_list()
                             return diskio.list_filename
                         }
-                        command_word[disk_name_length-4] = iso:'.'
+                        command_word[disk_name_length-4] = '.'
                     }
                 } else if only_programs and string.compare(command_word, filename_ptr)==0 {
                     diskio.lf_end_list()
-                    return err.set(iso:"Not a program")
+                    return err.set("Not a program")
                 }
             }
             diskio.lf_end_list()
@@ -165,7 +173,7 @@ main {
     sub iso_to_lowercase_petscii(uword str_ptr) -> ubyte {
         ubyte length=0
         while @(str_ptr)!=0 {
-            if @(str_ptr) >= iso:'a' and @(str_ptr) <= iso:'z'
+            if @(str_ptr) >= 'a' and @(str_ptr) <= 'z'
                 @(str_ptr) -= 32
             str_ptr++
             length++
@@ -179,17 +187,17 @@ main {
             ; only right before issuing the RUN command we switch back to petscii mode.
             txt.color2(1,6)     ; default white on blue
             void cx16.screen_mode(0, false)
-            txt.print(iso:"\x13LOAD\"")         ; home, load
+            txt.print("\x13LOAD\"")         ; home, load
             txt.print(filename_ptr)             ; is in ISO charset
-            txt.print(iso:"\",")
-            txt.chrout(iso:'0' + diskio.drivenumber)
-            txt.chrout(iso:':')
+            txt.print("\",")
+            txt.chrout('0' + diskio.drivenumber)
+            txt.chrout(':')
             for cx16.r0L in "\x13\r\x8frun:\r"     ; home, enter, iso_off, 'run', enter
                 cx16.kbdbuf_put(cx16.r0L)
             sys.exit(0)
         } else {
             txt.color(main.COLOR_HIGHLIGHT)
-            txt.print(iso:"Running: ")
+            txt.print("Running: ")
             txt.color(main.COLOR_NORMAL)
             txt.print(filename_ptr)
             txt.nl()
@@ -247,6 +255,6 @@ main {
                 }
             }
         }
-        return err.set(iso:"File not found")
+        return err.set("File not found")
     }
 }

commit 3c5db925c29fc49041ff1aa18c0dec7c1b85a536

Fix `copy` command
I think it broke in b1d6274

src/disk_commands.p8
@@ -230,10 +230,6 @@ io_error:
     sub cmd_mkdir() -> uword {
         if main.command_arguments_size==0
             return err.set("Missing arg: dirname")
-
-        diskio.list_filename[0] = 'm'
-        diskio.list_filename[1] = 'd'
-        diskio.list_filename[2] = ':'
         diskio.mkdir(main.command_arguments_ptr)
         print_disk_status()
         return true
@@ -277,7 +273,7 @@ io_error:
         if_cs {
             newfilename = main.command_arguments_ptr + space_idx + 1
             main.command_arguments_ptr[space_idx] = 0
-            diskio.list_filename[0] = 'c'
+            diskio.list_filename[0] = petscii:'c'
             diskio.list_filename[1] = ':'
             ubyte length = string.copy(newfilename, &diskio.list_filename+2)
             diskio.list_filename[length+2] = '='